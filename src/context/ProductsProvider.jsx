5be775b19d78f805dbba09c44a66c831c6bd6f4f
Alexen267-design <propriedade0@gmail.com>
2024-01-16 18:23:20 +0100

    fixing some null errors

--- src/context/ProductsProvider.jsx
+++ src/context/ProductsProvider.jsx
@@ -8,10 +8,10 @@ function ProductsProvider({ children }) {
     JSON.parse(localStorage.getItem("cart-list"))
   );
 
-  const subtotal =
-    cartList.length === 0
-      ? 0
-      : cartList.reduce((acc, curCart) => acc + parseInt(curCart.price), 0);
+  const subtotal = cartList?.reduce(
+    (acc, curCart) => acc + parseInt(curCart.price),
+    0
+  );
   function handleAddCartItem(cartItem) {
     setCartList((cartList) => [...cartList, cartItem]);
   }
